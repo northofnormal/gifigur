901d0d59f76fc4f11084a6520dc9957a10c4f9b2
Anne <northofnormal@gmail.com>
2013-10-12 18:16:27 -0400

    refactoring so it looks like big girl code

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,26 +1,37 @@
 class PicturesController < ApplicationController
 
   def index
-  end
-
-  def picture0
-    @picture = {
+    @pictures =[
+      {
       :emotion => "eyeroll",
       :url    => "http://media0.giphy.com/media/Rhhr8D5mKSX7O/giphy.gif"
-    }
-  end
-
-  def picture1
-    @picture = {
+      },
+      {
       :emotion  => "hungry",
       :url    => "http://www.reactiongifs.com/wp-content/uploads/2013/07/hungry.gif"
-    }
-  end
-
-  def picture2
-    @picture = {
+      },
+      {
       :emotion  => "haters gonna hate",
       :url    => "http://thesearethings.com/work/hatersgonnahate/img/hatersgonnahate-01.gif"
-    }
+      }
+    ]
+  end
+
+  def show
+    @pictures = [
+      {
+      :emotion => "eyeroll",
+      :url    => "http://media0.giphy.com/media/Rhhr8D5mKSX7O/giphy.gif"
+      },
+      {
+      :emotion  => "hungry",
+      :url    => "http://www.reactiongifs.com/wp-content/uploads/2013/07/hungry.gif"
+      },
+      {
+      :emotion  => "haters gonna hate",
+      :url    => "http://thesearethings.com/work/hatersgonnahate/img/hatersgonnahate-01.gif"
+      }
+    ]
+    @picture = @pictures[params[:id].to_i]
   end
 end
