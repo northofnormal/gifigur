4ce407a39fb336d2a8f02a47c48b43edb3c4d9a8
Anne <northofnormal@gmail.com>
2013-10-12 16:25:48 -0400

    fixed a syntax error in the html.erb

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,21 +4,21 @@ class PicturesController < ApplicationController
   end
 
   def picture0
-    @pictures = {
+    @picture = {
       :emotion => "eyeroll",
       :url    => "http://media0.giphy.com/media/Rhhr8D5mKSX7O/giphy.gif"
     }
   end
 
   def picture1
-    @pictures = {
+    @picture = {
       :emotion  => "hungry",
       :url    => "http://www.reactiongifs.com/wp-content/uploads/2013/07/hungry.gif"
     }
   end
 
   def picture2
-    @pictures = {
+    @picture = {
       :emotion  => "haters gonna hate",
       :url    => "http://thesearethings.com/work/hatersgonnahate/img/hatersgonnahate-01.gif"
     }
