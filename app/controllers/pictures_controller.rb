f3970ed2240db450b818744b0503a80db1996637
Anne <northofnormal@gmail.com>
2013-10-12 16:20:33 -0400

    hacked the individual pages will clean up later. I'm following the tutorial to remind myself of all the stuff I've learned in six weeks

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,19 +1,26 @@
 class PicturesController < ApplicationController
 
   def index
-  @pictures = [
-      {
-        :emotion => "eyeroll",
-        :url    => "http://media0.giphy.com/media/Rhhr8D5mKSX7O/giphy.gif"
-      },
-      {
-        :emotion  => "hungry",
-        :url    => "http://www.reactiongifs.com/wp-content/uploads/2013/07/hungry.gif"
-      },
-      {
-        :emotion  => "haters gonna hate",
-        :url    => "http://thesearethings.com/work/hatersgonnahate/img/hatersgonnahate-01.gif"
-      }
-    ]
+  end
+
+  def picture0
+    @pictures = {
+      :emotion => "eyeroll",
+      :url    => "http://media0.giphy.com/media/Rhhr8D5mKSX7O/giphy.gif"
+    }
+  end
+
+  def picture1
+    @pictures = {
+      :emotion  => "hungry",
+      :url    => "http://www.reactiongifs.com/wp-content/uploads/2013/07/hungry.gif"
+    }
+  end
+
+  def picture2
+    @pictures = {
+      :emotion  => "haters gonna hate",
+      :url    => "http://thesearethings.com/work/hatersgonnahate/img/hatersgonnahate-01.gif"
+    }
   end
 end
