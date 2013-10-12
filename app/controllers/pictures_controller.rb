2108b0a919fa35e1ad4e248077d47d94928bc89b
Anne <northofnormal@gmail.com>
2013-10-12 16:11:52 -0400

    hacked pics index, will improve later

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,19 @@
 class PicturesController < ApplicationController
 
   def index
-    @picture = Picture.all
+  @pictures = [
+      {
+        :emotion => "eyeroll",
+        :url    => "http://media0.giphy.com/media/Rhhr8D5mKSX7O/giphy.gif"
+      },
+      {
+        :emotion  => "hungry",
+        :url    => "http://www.reactiongifs.com/wp-content/uploads/2013/07/hungry.gif"
+      },
+      {
+        :emotion  => "haters gonna hate",
+        :url    => "http://thesearethings.com/work/hatersgonnahate/img/hatersgonnahate-01.gif"
+      }
+    ]
   end
 end
