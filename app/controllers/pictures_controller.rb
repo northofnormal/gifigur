57812f800222a21375e3354b9f4bde0e2bf7e32d
Anne <northofnormal@gmail.com>
2013-10-12 21:59:04 -0400

    refactored the form into big girl code

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,10 +9,21 @@ class PicturesController < ApplicationController
   end
 
   def new
-    
+    @picture = Picture.new
   end
 
   def create
-    render :text => "MOAR GIFS. Emotion: #{params[:emotion]}, URL: #{params[:url]}"
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to pictures_url
+    else
+      render :new
+    end    
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:emotion, :url)
   end
 end
